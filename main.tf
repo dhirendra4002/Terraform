95d8df41129b25c7d982804a2eaac7c73ff87ace
Ubuntu <ubuntu@ip-172-31-43-141.ap-south-1.compute.internal>
2022-09-22 08:51:47 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+ #access_key = "AKIARZP47FIYQH6DF5HX"
+ #secret_key = "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"
+  region     = "ap-south-1"
+}
+
+variable "privatekey" {
+  default = "/etc/ansible/dp2terraform.pem"
+}
+resource "aws_security_group" "splunk" {
+  name        = "terraform-splunk"
+  description = "Created by terraform"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-068257025f72f470d"
+  key_name      = "dp2terraform"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.splunk.name}"]
+    connection {
+    host        = "${self.public_ip}"
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("dp2terraform.pem")
+  }
+
+  tags = {
+    Name = "Prod"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "ping -c 10 8.8.8.8",
+    ]
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -u ubuntu -i ${aws_instance.web.public_ip}, --private-key ${var.privatekey} jenkins.yml"
+  }
+}
+output "web_ip" {
+  value = aws_instance.web.public_ip
+}
